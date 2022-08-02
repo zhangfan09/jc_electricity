71b3a9f8bfab496b88c859f62999f6383933def2
张帆 <15712870334@163.com>
2022-08-02 23:25:45 +0800

    第二次提交验证是否需要令牌

--- demo/src/main/java/com/jc/demo/controller/DemoController.java
+++ demo/src/main/java/com/jc/demo/controller/DemoController.java
@@ -10,6 +10,7 @@ public class DemoController {
     @RequestMapping("index")
     @ResponseBody
     public  String Hello(){
+        System.out.print("+++++++++++++++++++++++++++++++++");
         return "welcome to jc-project";
     }
 }
