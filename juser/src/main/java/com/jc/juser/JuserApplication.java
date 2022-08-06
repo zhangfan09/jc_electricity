7003ec348658f2d8ce337dc05fb5d633d5a960dd
muying <17689853176@163.com>
2022-08-06 16:31:40 +0800

    提交用户信息模块

---
+++ juser/src/main/java/com/jc/juser/JuserApplication.java
@@ -0,0 +1,13 @@
+package com.jc.juser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuserApplication.class, args);
+    }
+
+}
---
+++ juser/src/test/java/com/jc/juser/JuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.juser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
