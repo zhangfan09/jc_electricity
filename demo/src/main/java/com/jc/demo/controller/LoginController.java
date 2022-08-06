36964602c1ef2ecae3fd5e57e0de09093c53fe37
muying <17689853176@163.com>
2022-08-06 10:21:07 +0800

    登录功能假性开发

--- demo/src/main/java/com/jc/demo/controller/LoginController.java
+++ demo/src/main/java/com/jc/demo/controller/LoginController.java
@@ -8,6 +8,13 @@ public class LoginController {
 
     @RequestMapping("login")
     public String Login(){
+        System.out.print("登录功能开发");
         return "当前登录页面";
+    }
+
+    @RequestMapping("register")
+    public String register(){
+        System.out.print("注册功能开发");
+        return "跳转至登录";
     }
 }
